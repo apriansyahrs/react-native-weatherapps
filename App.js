e29f4c9df2caa876f3e092753736d45344191213
apriansyahrs <apriansyahrizs@gmail.com>
2024-06-23 23:25:58 +0700

    develop search

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
+// Import axios, BASE_URL, dan API_KEY
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
+  const searchWeather = (location) => {
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <WeatherSearch />
+      <WeatherSearch searchWeather={searchWeather} />
       <WeatherInfo />
     </View>
   )
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'eb4d9d0b8385f9bed9d426be7f04dc8f';
